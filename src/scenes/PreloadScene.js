ce8a15baee98c4a92537839b7b11b19017e4edb2
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2026-02-06 01:20:50 +0300

    bug fix

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -6,8 +6,8 @@ export class PreloadScene extends Phaser.Scene {
     preload() {
         console.log('PreloadScene started');
         
-        // Fix CORS issues
-        this.load.crossOrigin = 'anonymous';
+        // Fix CORS issues (not needed for local assets, but good for hygiene)
+        // this.load.crossOrigin = 'anonymous';
         this.load.baseURL = ''; // Ensure no double base path issues
 
         // Handle loading errors
@@ -18,18 +18,18 @@ export class PreloadScene extends Phaser.Scene {
         });
 
         // Load Images
-        this.load.image('player', 'https://labs.phaser.io/assets/sprites/ship.png');
-        this.load.image('bullet', 'https://labs.phaser.io/assets/sprites/bullets/bullet1.png');
-        this.load.image('enemy_chaser', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-        this.load.image('enemy_sprinter', 'https://labs.phaser.io/assets/sprites/ufo.png');
-        this.load.image('background', 'https://labs.phaser.io/assets/skies/space3.png');
-        this.load.image('spark', 'https://labs.phaser.io/assets/particles/blue.png'); 
-        this.load.image('fire', 'https://labs.phaser.io/assets/particles/fire.png');
+        this.load.image('player', './assets/sprites/ship.png');
+        this.load.image('bullet', './assets/sprites/bullets/bullet1.png');
+        this.load.image('enemy_chaser', './assets/sprites/phaser-dude.png');
+        this.load.image('enemy_sprinter', './assets/sprites/ufo.png');
+        this.load.image('background', './assets/skies/space3.png');
+        this.load.image('spark', './assets/particles/blue.png'); 
+        this.load.image('fire', './assets/particles/fire.png');
         
         // Load Audio
-        this.load.audio('shoot', 'https://labs.phaser.io/assets/audio/SoundEffects/pistol.wav');
-        this.load.audio('explosion', 'https://labs.phaser.io/assets/audio/SoundEffects/explosion.mp3');
-        this.load.audio('bgm', 'https://labs.phaser.io/assets/audio/tech/Asylum.mp3');
+        this.load.audio('shoot', './assets/audio/SoundEffects/pistol.wav');
+        this.load.audio('explosion', './assets/audio/SoundEffects/explosion.mp3');
+        this.load.audio('bgm', './assets/audio/tech/Asylum.mp3');
 
         // XP Orb - Generate texture programmatically
         const xpGfx = this.make.graphics({ x: 0, y: 0, add: false });
